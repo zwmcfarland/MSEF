acb43a48adee16ad770e6ad1fe49189360d579d6
Zachary McFarland <zwmcfarland@gmail.com>
2014-11-15 21:35:19 +0000

    blank

--- src/function/Data_Source.php
+++ src/function/Data_Source.php
@@ -8,10 +8,10 @@
         
         /* check connection */
         if (mysqli_connect_errno()) {
-            printf("Connect failed: %s\n", mysqli_connect_error());
+            echo "Connect failed: %s\n" . mysqli_connect_error();
             exit();
         }
 
-        return $connection;
+        return $connection; 
     }
 ?>
