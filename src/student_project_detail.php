c79595f7268b6973f03b5e1d0413c7844201f36b
Noel Minchow <nowlminchow@gmail.com>
2014-11-15 22:08:40 +0000

    added social media fixed spacing on forms created project detail page

---
+++ src/student_project_detail.php
@@ -0,0 +1,55 @@
+<?php
+    include("function/headerfooter.php");
+    include("function/user.php");
+    incHeader('MSEF | Home');
+    
+    /* --- Queries --- */
+    /* --- END: Queries ---*/
+    
+    /* --- Security --- */
+    /* --- END: Security --- */
+?>
+<div class="col-lg-12">
+    <!-- Users Detials -->
+    <div class="col-md-6 col-md-offset-3">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h3 class="panel-title">Project Information</h3>
+            </div>
+
+            <div class="panel-body">
+                <dl class="dl-horizontal">
+                    <dt>Name</dt>
+                    <dd>Project 1</dd>
+                </dl>
+                <dl class="dl-horizontal">
+                    <dt>Description</dt>
+                    <dd>McFarland</dd>
+                </dl>
+                <dl class="dl-horizontal">
+                    <dt>Need Electric</dt>
+                    <dd>Louisville Public School</dd>
+                </dl>
+                <dl class="dl-horizontal">
+                    <dt>Description</dt>
+                    <dd>Louisville Public School</dd>
+                </dl>
+                <dl class="dl-horizontal">
+                    <dt>Abstract</dt>
+                    <dd>Louisville Public School</dd>
+                </dl>
+                <dl class="dl-horizontal">
+                	<dt></dt>
+                    <dd><a href="#">Edit Project</a></dd>
+                </dl>
+                
+            </div>
+        </div>
+    </div>
+    <!-- END: Users Detials -->
+
+   
+</div>
+<?php
+    incFooter();
+?>
--- src/user_login.php
+++ src/user_login.php
@@ -38,10 +38,10 @@
 				<!-- login form box -->
 				<form method="post" action="user_login.php" name="loginform">
 				
-				    <label for="login_input_username">Username</label>
+				    <label for="login_input_username">Username</label><br>
 				    <input id="login_input_username" class="login_input" type="text" name="user_name" required />
 				
-				    <label for="login_input_password">Password</label>
+				    <label for="login_input_password">Password</label><br>
 				    <input id="login_input_password" class="login_input" type="password" name="user_password" autocomplete="off" required />
 					<br><br>
 				    <input type="submit"  name="login" value="Log in" />
