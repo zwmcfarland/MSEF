2f3ad0c0f57a671466fa395d7206095364d736af
Zachary McFarland <zwmcfarland@gmail.com>
2014-11-15 18:43:02 +0000

    Updated the header footer file.

--- src/index.php
+++ src/index.php
@@ -8,10 +8,26 @@
     /* --- Security --- */
     /* --- END: Security --- */
 ?>
-    <div class="span12" style="width: 100%;">
-        <div class="row-fluid">
-        </div>
-    </div>
+<div class="col-lg-12">
+    <div class="col-lg-5 col-md-offset-6">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h3 class="panel-title">About</h3>
+            </div>
+            <div class="panel-body">
+                <p>
+                    The annual Metropolitan Science and Engineering Fair (MSEF)/Nebraska Junior Academy of Sciences(NJAS) is open to all
+                    greater Omaha-area students in grades 7-12. It is held at the Henry Doorly Zoo and Aquarium Conference Center. An awards ceremony
+                    will follow. The MSEF/NJAS gives students an opportunity to present and display their research projects and have their projects
+                    judged by area college instructors and practicing professionals in the projects field.
+                </p>
+                <p>
+                    MSEF is affiliated with the Nebraska Junior Academy of Sciences (NJAS). Further information about this year's Fair is found in this year's Booklet
+                </p>
+           </div>
+       </div>
+   </div>
+</div>
 <?php
     incFooter();
 ?>
