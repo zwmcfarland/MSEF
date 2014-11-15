7c407edf9603dccc1a543407231b5433a79b433b
Noel Minchow <nowlminchow@gmail.com>
2014-11-15 23:01:12 +0000

    Added details page for awards Updated header footer links Added edit button to project details

--- src/award_detail.php
+++ src/award_detail.php
@@ -14,28 +14,20 @@
     <div class="col-md-6 col-md-offset-3">
         <div class="panel panel-default">
             <div class="panel-heading">
-                <h3 class="panel-title" style="display:inline-block">Project Information</h3><a><i style="float:right" class="fa fa-pencil-square-o fa-2x"></i></a>
+                <h3 class="panel-title" style="display:inline-block">Award Information</h3><a><i style="float:right" class="fa fa-pencil-square-o fa-2x"></i></a>
             </div>
 
             <div class="panel-body">
                 <dl class="dl-horizontal">
                     <dt>Name</dt>
-                    <dd>Project 1</dd>
+                    <dd>Award 1</dd>
                 </dl>
                 <dl class="dl-horizontal">
                     <dt>Description</dt>
                     <dd>McFarland</dd>
                 </dl>
                 <dl class="dl-horizontal">
-                    <dt>Need Electric</dt>
-                    <dd>Louisville Public School</dd>
-                </dl>
-                <dl class="dl-horizontal">
-                    <dt>Description</dt>
-                    <dd>Louisville Public School</dd>
-                </dl>
-                <dl class="dl-horizontal">
-                    <dt>Abstract</dt>
+                    <dt>Reward</dt>
                     <dd>Louisville Public School</dd>
                 </dl>
             </div>
